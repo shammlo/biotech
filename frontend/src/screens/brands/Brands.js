3585bb91c4915faaab9c552f8d0247bc2dc6a004
zhir-aryan <zhiraryan@gmail.com>
2022-02-19 13:50:06 +0300

    axios

--- frontend/src/screens/brands/Brands.js
+++ frontend/src/screens/brands/Brands.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import "./brands.css"
 import Card from "../../components/brand-card/Card"
 function Brands() {
+    const [brand, setbrand] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5555/api/brand/").then((response) => {
+            setbrand(response.data)
+
+        });
+    }, []);
     return (
         <div id='brand-section' className='brands-section'>
             <div className='container-brand '>
                 <div class="ccontainer">
+                    {
+                        brand.map((e) => (
+                            <Card />
+                        ))
+                    }
 
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+
 
                 </div>
             </div>
